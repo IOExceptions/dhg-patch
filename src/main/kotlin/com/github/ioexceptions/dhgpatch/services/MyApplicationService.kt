b4aac5ca17978d2af84fb3915cd903a9f60d6270
GitHub Action <action@github.com>
2022-03-24 06:50:06 +0000

    Template cleanup

--- src/main/kotlin/com/github/ioexceptions/dhgpatch/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/ioexceptions/dhgpatch/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.ioexceptions.dhgpatch.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.ioexceptions.dhgpatch.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/ioexceptions/dhgpatch/services/MyProjectService.kt
+++ src/main/kotlin/com/github/ioexceptions/dhgpatch/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.ioexceptions.dhgpatch.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.ioexceptions.dhgpatch.MyBundle
 
 class MyProjectService(project: Project) {
 
